2c77ca93a1e74fc3c59ea3b94343d1ef559c011b
Viktor Sokolov <71142348+Viktor-Sok@users.noreply.github.com>
2022-11-10 15:12:05 +0300

    Update template-metaprogramming.cpp

--- template-metaprogramming/template-metaprogramming.cpp
+++ template-metaprogramming/template-metaprogramming.cpp
@@ -14,11 +14,12 @@ template<int N1, int ...Tail>
 constexpr auto num_v<N1, N1, Tail ...> = {N1, Tail ...}; // auto will be std::initializer_list
 //--------------------------------------------------------------------------//
 
+template M<typename T>
 class Numbers {
 private:
-    std::vector<int> v;
+    std::vector<T> v;
 public:
-    Numbers(initializer_list<int> list): v(list){} // std::initializer_list is used to initialize v the ususal way v = {1, 2, 3}
+    Numbers(initializer_list<T> list): v(list){} // std::initializer_list is used to initialize v the ususal way v = {1, 2, 3}
     void printNumbers() {
         for (auto e : v) {
              cout << e << " ";
@@ -27,7 +28,7 @@ public:
 };
 
 int main() {
-    Numbers n[]  =  {num_v<1, 5>, num_v<3, 6>, num_v<7, 8>}; // all the sequances num_v will be calculated in compile time
+    Numbers<int> n[]  =  {num_v<1, 5>, num_v<3, 6>, num_v<7, 8>}; // all the sequances num_v will be calculated in compile time
     //Numbers n[] = {{1, 2, 3, 4, 5}, {3, 4, 5, 6}, {7, 8}}; // the line above will get us the result equivalent to this
     for (auto e : n) {
         e.printNumbers();
